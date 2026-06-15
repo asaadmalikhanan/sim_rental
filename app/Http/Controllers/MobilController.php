3cb5f5491f0f1c3d1440eac695c4e85507cb8e0d
asaadmalikhanan <asaadmalikhanan@gmail.com>
2026-06-15 19:22:43 +0700

    add filter merk mobil

--- app/Http/Controllers/MobilController.php
+++ app/Http/Controllers/MobilController.php
@@ -1,23 +1,24 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Models\Mobil;
-
 class MobilController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $mobils = Mobil::latest()->paginate(10);
+        $query = Mobil::latest();
+
+        if ($request->has('merk') && $request->merk != '') {
+            $query->where('merk', $request->merk);
+        }
+
+        $mobils = $query->paginate(10)->withQueryString();
         return view('mobil.index', compact('mobils'));
     }
-
     public function create()
     {
         return view('mobil.create');
     }
-
     public function store(Request $request)
     {
         $request->validate([
@@ -33,18 +34,14 @@ class MobilController extends Controller
             'jenis_bbm'       => 'required|in:bensin,solar,listrik,hybrid',
             'kondisi'         => 'required|in:sangat_baik,baik,cukup,kurang',
         ]);
-
         Mobil::create($request->all());
-
         return redirect()->route('mobil.index')
                          ->with('success', 'Data mobil berhasil ditambahkan!');
     }
-
     public function edit(Mobil $mobil)
     {
         return view('mobil.edit', compact('mobil'));
     }
-
     public function update(Request $request, Mobil $mobil)
     {
         $request->validate([
@@ -60,17 +57,13 @@ class MobilController extends Controller
             'jenis_bbm'       => 'required|in:bensin,solar,listrik,hybrid',
             'kondisi'         => 'required|in:sangat_baik,baik,cukup,kurang',
         ]);
-
         $mobil->update($request->all());
-
         return redirect()->route('mobil.index')
                          ->with('success', 'Data mobil berhasil diperbarui!');
     }
-
     public function destroy(Mobil $mobil)
     {
         $mobil->delete();
-
         return redirect()->route('mobil.index')
                          ->with('success', 'Data mobil berhasil dihapus!');
     }
