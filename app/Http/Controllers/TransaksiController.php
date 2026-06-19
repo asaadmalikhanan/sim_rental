94d2ab220b16e5eac14c894dddcd6ca2575614d9
asaadmalikhanan <asaadmalikhanan@gmail.com>
2026-06-19 09:33:01 +0700

    update pengembalian to show only active transactions

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -1,28 +1,26 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Models\Transaksi;
 use App\Models\Mobil;
 use App\Models\Pelanggan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
-
 class TransaksiController extends Controller
 {
     public function index()
     {
-        $transaksis = Transaksi::with(['pelanggan', 'mobil'])->latest()->paginate(10);
+        $transaksis = Transaksi::with(['pelanggan', 'mobil'])
+                        ->where('status', 'aktif')
+                        ->latest()
+                        ->paginate(10);
         return view('transaksi.index', compact('transaksis'));
     }
-
     public function create()
     {
         $pelanggans = Pelanggan::all();
         $mobils = Mobil::where('status', 'tersedia')->get();
         return view('transaksi.create', compact('pelanggans', 'mobils'));
     }
-
     public function store(Request $request)
     {
         $request->validate([
@@ -32,13 +30,11 @@ class TransaksiController extends Controller
             'tanggal_selesai' => 'required|date|after:tanggal_mulai',
             'catatan'         => 'nullable|string',
         ]);
-
         $mobil = Mobil::findOrFail($request->mobil_id);
         $lamaSewa = \Carbon\Carbon::parse($request->tanggal_mulai)
                         ->diffInDays($request->tanggal_selesai);
         $totalBayar = $lamaSewa * $mobil->harga_sewa;
         $kode = 'TRX-' . date('Ymd') . '-' . strtoupper(Str::random(4));
-
         Transaksi::create([
             'kode_transaksi'  => $kode,
             'pelanggan_id'    => $request->pelanggan_id,
@@ -50,26 +46,19 @@ class TransaksiController extends Controller
             'status'          => 'aktif',
             'catatan'         => $request->catatan,
         ]);
-
-        // Update status mobil jadi disewa
         $mobil->update(['status' => 'disewa']);
-
         return redirect()->route('transaksi.index')
                          ->with('success', 'Transaksi berhasil dibuat!');
     }
-
     public function show(Transaksi $transaksi)
     {
         $transaksi->load(['pelanggan', 'mobil']);
         return view('transaksi.show', compact('transaksi'));
     }
-
     public function destroy(Transaksi $transaksi)
     {
-        // Kembalikan status mobil
         $transaksi->mobil->update(['status' => 'tersedia']);
         $transaksi->delete();
-
         return redirect()->route('transaksi.index')
                          ->with('success', 'Transaksi berhasil dihapus!');
     }
